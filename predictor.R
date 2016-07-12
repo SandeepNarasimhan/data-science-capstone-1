f6da72fad579353e226a5890d2d52f31591e3b9e
Igor Goltsov <riversy@gmail.com>
2016-07-13 01:15:20 +0300

    Add # Some optimisations but need to test ML before decide to complete it

--- predictor.R
+++ predictor.R
@@ -8,6 +8,7 @@ if (!is_ngrams_ready(ngrams_n)){
     
     print("Load Data")
     timer = system.time({
+        # lines = get_combined_data()    
         lines = get_combined_sample()    
     })
     prepare_ngrams(lines, ngrams_n)
@@ -21,8 +22,8 @@ find_inside_ngrams = function(words, ngrams_n){
     search_query = paste(words[start_index:end_index], collapse = "_")
     search_pattern = paste(c("^", search_query, "_"), collapse = "")
     ngrams_df = get_ngrams(ngrams_n)
-    selected_df = select(ngrams_df, starts_with("terms")) %>% 
-        filter(grepl(search_pattern, terms)) %>%
+    selected_df = filter(ngrams_df, grepl(search_pattern, terms)) %>% 
+        select(starts_with("terms")) %>%
         arrange(desc(terms_counts))
     
     selected_df$terms = gsub(paste(c(search_query, "_"), collapse = ""), "", selected_df$terms)
@@ -50,27 +51,63 @@ predict = function(phrase){
 # Test Predictor 
 ###
 
-head(predict("The guy in front of me just bought a pound of bacon, a bouquet, and a case of"), 10)
-
-head(predict("You're the reason why I smile everyday. Can you follow me please? It would mean the"), 10)
-
-head(predict("Hey sunshine, can you follow me and make me the"), 10)
-
-head(predict("Very early observations on the Bills game: Offense still struggling but the"), 10)
-
-head(predict("Go on a romantic date at the"), 10)
-
-head(predict("Well I'm pretty sure my granny has some old bagpipes in her garage I'll dust them off and be on my"), 10)
-
-head(predict("Ohhhhh #PointBreak is on tomorrow. Love that film and haven't seen it in quite some"), 10)
-
-head(predict("After the ice bucket challenge Louis will push his long wet hair out of his eyes with his little"), 10)
-
-head(predict("Be grateful for the good times and keep the faith during the"), 10)
-
-head(predict("If this isn't the cutest thing you've ever seen, then you must be"), 10)
+# Question 1
+prediction = predict("I'll be there for you, I'd live and I'd")
+matched = prediction[prediction %in% c('sleep', 'die', 'eat', 'give')]
+print(matched)
 
 
+# Question 2
+prediction = predict("Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his")
+matched = prediction[prediction %in% c('spiritual', 'financial', 'marital', 'horticultural')]
+print(matched)
 
-# print(clean.lines)
+
+# Question 3
+prediction = predict("I'd give anything to see arctic monkeys this")
+matched = prediction[prediction %in% c('morning', 'decade', 'month', 'weekend')]
+print(matched)
+
+
+# Question 4
+prediction = predict("Talking to your mom has the same effect as a hug and helps reduce your")
+matched = prediction[prediction %in% c('sleepiness', 'hunger', 'stress', 'happiness')]
+print(matched)
+
+
+# Question 5
+prediction = predict("When you were in Holland you were like 1 inch away from me but you hadn't time to take a")
+matched = prediction[prediction %in% c('minute', 'look', 'picture', 'walk')]
+print(matched)
+
+
+# Question 6
+prediction = predict("I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the")
+matched = prediction[prediction %in% c('matter', 'account', 'incident', 'case')]
+print(matched)
+
+
+# Question 7
+prediction = predict("I can't even hold an uneven number of bags of groceries in each")
+matched = prediction[prediction %in% c('toe', 'hand', 'arm', 'finger')]
+print(matched)
+
+
+# Question 8
+prediction = predict("Every inch of you is perfect from the bottom to the")
+matched = prediction[prediction %in% c('center', 'top', 'middle', 'side')]
+print(matched)
+
+
+# Question 9
+prediction = predict("I’m thankful my childhood was filled with imagination and bruises from playing")
+matched = prediction[prediction %in% c('outside', 'inside', 'daily', 'weekly')]
+print(matched)
+
+
+# Question 10
+prediction = predict("I like how the same people are in almost all of Adam Sandler's")
+matched = prediction[prediction %in% c('pictures', 'novels', 'movies', 'stories')]
+print(matched)
+
 
--- utils/ngrams.R
+++ utils/ngrams.R
@@ -9,14 +9,32 @@ get_ngrams_file = function(ngrams_n){
     paste(c(ngrams_n, "RDS"), collapse = ".")
 }
 
+get_short_ngrams_path = function(ngrams_n, first_sign, second_sign = "_"){
+    ngrams_file = get_ngrams_file(ngrams_n)
+    paste(c(ngrams_folder_name,  first_sign, second_sign, ngrams_file), collapse = "/")
+}
+
 get_ngrams_path = function(ngrams_n){
     ngrams_file = get_ngrams_file(ngrams_n)
     paste(c(ngrams_folder_name, ngrams_file), collapse = "/")
 }
 
 get_ngrams = function(ngrams_n){
-    ngrams_path = get_ngrams_path(ngrams_n)
-    readRDS(ngrams_path)
+    global_var_name = paste(c("ngram", ngrams_n), collapse = "_")
+    if (!exists(global_var_name)){
+        ngrams_path = get_ngrams_path(ngrams_n)
+        assign(global_var_name, readRDS(ngrams_path), envir = .GlobalEnv)    
+    }
+    get(global_var_name)
+}
+
+save_short_ngrams = function(df, ngrams_n, first_sign, second_sign = "_"){
+    ensure_dir_exists(ngrams_folder_name)
+    ngrams_path = get_short_ngrams_path(ngrams_n, first_sign, second_sign)
+    if (file.exists(ngrams_path)){
+        file.remove(ngrams_path)
+    }
+    saveRDS(df, ngrams_path)   
 }
 
 save_ngrams = function(df, ngrams_n){
@@ -72,6 +90,7 @@ prepare_ngrams = function(lines, ngrams_n){
         timer = system.time({
             tokens = get_tokens(lines)
             vocab = create_vocabulary(tokens, c(n, n), stopwords = words_to_remove)
+            vocab = prune_vocabulary(vocab, term_count_min = 2)
             save_ngrams(vocab$vocab, n)  
             rm(vocab)
             rm(tokens)
@@ -79,3 +98,24 @@ prepare_ngrams = function(lines, ngrams_n){
         print(paste(c("Done", n, "grams"), collapse = " "))
     }
 }
+
+# transform_ngram_into_short = function(ngrams_n){
+#     
+#     
+#     df = get_ngrams(ngrams_n)
+#     
+#     
+#     print(head(df))
+#     
+#     
+#     
+#     
+#     
+# }
+# 
+# transform_ngrams_into_short = function(){
+#     for (n in ngrams_n){
+#         transform_ngram_into_short(n)
+#     }    
+# }
+# 
